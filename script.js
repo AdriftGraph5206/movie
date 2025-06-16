a9a9362fe70395a913c7ea9f8251c169a4762c97
AdriftGraph5206 <85671732+AdriftGraph5206@users.noreply.github.com>
2025-06-16 15:42:48 +0300

    asdas
    asd

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// padaryti movie review site savaites darbas kuo daugiau tuo geriau 
+function fasa(){
+let rgb1 = Math.floor(Math.random() * 256);
+let rgb2 = Math.floor(Math.random() * 256);
+let rgb3 = Math.floor(Math.random() * 256);
+return `rgb(${rgb1}, ${rgb2}, ${rgb3})`
+}
+document.body.style.backgroundColor = fasa()
